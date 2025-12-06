0bf37f82065f7b7bbea208f0e0cfc9f6610fe107
Hawk <hawk@zodeus.com>
2025-12-05 19:54:54 -0800

    little stuff

--- Grafted/Source/Sim/Combat/CombatHandler.cs
+++ Grafted/Source/Sim/Combat/CombatHandler.cs
@@ -237,6 +237,7 @@ public class CombatHandler : IDisposable
         {
             UseBloodPotion(p, pawn);
             pawn.Equipment.UnEquip(p);
+            Core.Context.Achievements.OnItemUsed(pawn, p);
         }
     }
 
